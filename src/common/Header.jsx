0e55864dc15b74c08b159e59a334db0295ec5225
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-06 21:23:48 +0530

    add image

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <div className='pt-4 overflow-hidden min-[1921px]:max-w-[1920px] mx-auto'>
             <div className='container flex justify-between items-center'>
                 <div className='relative z-20'>
-                    <a href="#" className='text-[#FF002A] text-4xl font-godzilla font-normal'><img className='max-w-[105px]' src={logo} alt="" /></a>
+                    <a href="#" className='text-black text-4xl font-normal'><img className='max-w-[105px]' src={logo} alt="logo" /></a>
                 </div>
                 <div className={`flex gap-10 max-lg:flex-col max-lg:justify-center max-lg:items-center ${open ? 'max-lg:z-10 max-lg:translate-x-0 max-lg:bg-black max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:h-full max-lg:w-full max-lg:bg-opacity-80 max-lg:transition-opacity duration-500 ease-linear blur-[80%] justify-center items-center' : 'max-lg:-left-full max-lg:hidden duration-500 ease-linear'}`}>
                     {HEADER_ITEM.map((obj, i) => (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import CustomButton from '../common/CustomButton'
-import { ButtonArrowIcon } from '../utils/Icons'
-import heroImage from '../assets/images/webp/hero-image.webp'
-import dotsImage from '../assets/images/webp/dodo-logo.webp'
-import Header from '../components/Header'
-
+import React from 'react';
+import CustomButton from '../common/CustomButton';
+import {ButtonArrowIcon} from '../utils/Icons';
+import heroImage from '../assets/images/webp/hero-image.webp';
+import dotsImage from '../assets/images/webp/dodo-logo.webp';
+import Header from '../common/Header';
 
 const Hero = () => {
   return (
-    <div className='bg-hero-bg bg-cover bg-no-repeat max-2xl:bg-contain max-xl:bg-cover bg-top '>
+    <div className="bg-hero-bg bg-cover bg-no-repeat max-2xl:bg-contain max-xl:bg-cover bg-top bg-light-blue">
       <Header/>
-      <div className='container pt-[109px] max-lg:pt-20 max-md:pt-12 max-sm:pt-8'>
-        <div className='flex max-lg:flex-col gap-8'>
-          <div className='w-6/12 max-lg:w-full max-lg:text-center flex justify-center'>
+      <div className="container pt-[109px] max-lg:pt-20 max-md:pt-12 max-sm:pt-8">
+        <div className="flex max-lg:flex-col gap-8">
+          <div className="lg:w-6/12 w-full text-center lg:text-left flex justify-center">
             <div>
-              <h1 className='text-custom-xl font-normal text-white max-w-[612px] leading-custom-xl max-lg:text-5xl max-md:text-4xl max-sm:text-3xl'>Easily Compare <span className='font-bold'>Energy, Gas, and Internet</span>
-                Plans
+              <h1 className="text-custom-xl font-normal text-white leading-custom-xl max-lg:text-5xl max-md:text-4xl max-sm:text-3xl">
+                Easily Compare <span className="font-bold">Energy, Gas, and Internet</span> Plans
               </h1>
-              <p className='font-normal text-base text-white max-w-[506px] pt-4 pb-[42px] leading-custom-2xl max-lg:pb-6 max-md:text-sm max-sm:pt-2'>Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place.</p>
-              <div className='flex pl-[22px] p-[9px] rounded-full rounded-tl-none bg-white items-center max-w-[476px] justify-between max-sm:pl-4 max-lg:mx-auto'>
-                <input type="text" placeholder='Start typing your address' className='placeholder:text-gray-500 placeholder:text-base placeholder:font-normal outline-none placeholder:max-sm:text-sm' />
-                <CustomButton CustomButton="Compare" buttonSpan={<ButtonArrowIcon/>} />
+              <p className="text-base font-normal text-white leading-custom-2xl pt-4 pb-[42px] max-lg:pb-6 max-md:text-sm max-sm:pt-2">
+                Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place.
+              </p>
+              <div className="flex items-center bg-white rounded-full p-[9px] max-w-[476px] mx-auto lg:mx-0">
+                <label htmlFor="addressInput" className="sr-only">
+                  Address
+                </label>
+                <input
+                  id="addressInput"
+                  type="text"
+                  placeholder="Start typing your address"
+                  className="flex-grow placeholder-gray-500 text-base font-normal outline-none max-sm:text-sm"
+                />
+                <CustomButton label="Compare" icon={<ButtonArrowIcon />} />
               </div>
             </div>
-            <div>
-              <p className='font-normal text-black text-custom-lg max-md:text-3xl max-sm:text-2xl max-lg:pt-6 '>More than <span className='text-light-blue'>80,000</span>+ companies trust bill central</p>
+            <div className="pt-6">
+              <p className="font-normal text-black !text-custom-lg max-md:text-3xl max-sm:text-2xl">
+                More than <span className="text-light-blue">80,000</span>+ companies trust Bill Central
+              </p>
             </div>
           </div>
-          <div className='w-6/12 max-lg:w-full max-lg:items-center max-lg:flex max-lg:justify-center'>
-            <div className='relative'>
-              <img className='absolute max-sm:hidden' src={dotsImage} alt="dots" />
-              <img className='max-w-[558px] w-full relative' src={heroImage} alt="hero-image" />
-            </div>
+          <div className="lg:w-6/12 w-full flex justify-center items-center relative">
+            <img className="absolute left-[40%'" src={dotsImage} alt="Background dots" />
+            <img className="max-w-[558px] w-full relative" src={heroImage} alt="Hero section" />
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </div
+  );
+};
 
-export default Hero
+export default Hero;
