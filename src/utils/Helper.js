7d0378ec74052f3199a8b2d3be25b78693d130c5
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-09 14:51:58 +0530

    responsive done

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -26,6 +26,16 @@ export const HEADER_LIST = [
  
 
 ];
+export const LOGOS = [
+    { src: './assets/images/webp/first-energy-logo.webp', alt: 'first-energy' },
+    { src: './assets/images/webp/blue-nrg-logo.webp', alt: 'blue-nrg' },
+    { src: './assets/images/webp/dodo-logo.webp', alt: 'dodo-logo' },
+    { src: './assets/images/webp/origin-logo.webp', alt: 'origin-logo' },
+    { src: './assets/images/webp/ovo-logo.webp', alt: 'ovo-logo' },
+    { src: './assets/images/webp/sumo-logo.webp', alt: 'sumo-logo' },
+    { src: './assets/images/webp/agl-logo.webp', alt: 'agl-logo' },
+];
+    
 export const BENEFITS = [
     'Save money by finding cheaper deals.',
     'Get better customer service.',
