10adeb3d8e36853a5970903e1dce76ab9d5c18b7
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-12-09 15:24:05 +0530

    check spacing

--- src/components/CompaniesLogo.jsx
+++ src/components/CompaniesLogo.jsx
@@ -5,11 +5,11 @@ const CompaniesLogo = () => {
     <div>
       <div className="container">
         <h2 className='font-normal text-[38px] max-md:text-3xl text-[#003459] max-w-[548px] leading-[41.8px] max-2xl:-mt-[75px] max-xl:-mt-0 max-xl:pt-10 max-xl:text-center max-xl:mx-auto'>More than <span className='text-[#00A8E8]'>80,000</span>+ companies trust bill central </h2>
-        <div className="flex pt-[63px] max-md:pt-10 max-lg:flex-wrap max-lg:justify-center">
+        <div className="flex pt-[63px] max-xl:pt-12 max-md:pt-10 max-lg:flex-wrap max-lg:justify-center">
           {LOGOS.map((LOGO, index) => (
             <img
               key={index}
-              className={`pointer-events-none max-w-[126.67px] h-[62px] ${index !== LOGOS.length - 1 ? 'mr-[51px] max-xl:mr-[30px] max-lg:mr-[38px]' : ''
+              className={`pointer-events-none max-w-[126.67px] h-[62px] ${index !== LOGOS.length - 1 ? 'mr-[45px] max-xl:mr-[30px] max-lg:mr-[38px]' : ''
                 } ${index >= 2 ? 'max-md:mt-10' : ''}`}
               src={LOGO.src}
               alt={LOGO.alt}
--- src/components/SimpleSteps.jsx
+++ src/components/SimpleSteps.jsx
@@ -6,7 +6,7 @@ const SimpleSteps = () => {
     <div className='max-lg:pb-[44px]'>
       <div className="2xl:container">
         <h2 className='text-[#00171F] font-normal text-5xl max-md:text-3xl leading-[52.8px] pb-4 text-center'>Simple Steps to <span className='font-bold'> Switch</span></h2>
-        <p className='font-normal text-base text-[#4B4B4B] max-w-[610px] mx-auto text-center pb-[58px] max-md:pb-10 max-md:mx-auto max-md:px-3'>Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum</p>
+        <p className='font-normal text-base text-[#4B4B4B] max-w-[610px] leading-[25.6px] mx-auto text-center pb-[58px] max-md:pb-10 max-md:mx-auto max-md:px-3'>Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum</p>
         <div className="flex flex-wrap max-xl:justify-center">
           {STEPS.map((STEP, index) => (
             <img
